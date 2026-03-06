53dd532049ad38ee9908e7309a7f664c0c7b1ea0
DELL <DELL@DESKTOP-TKMLJJN>
2026-03-06 16:47:24 +0530

    Base Class Updated

--- src/test/java/test/Base_Simple_Test.java
+++ src/test/java/test/Base_Simple_Test.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -17,7 +18,13 @@ public class Base_Simple_Test {
 		ConfigReader config = new ConfigReader();
 
 		String URL = config.getURL();
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless=new");
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
+	    options.addArguments("--disable-gpu");
+
+	    driver = new ChromeDriver(options);
 		driver.get(URL);
 		driver.manage().window().maximize();
 
